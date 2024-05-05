a4d31fd3081297760d5661d968a1061dce072b3d
guido88 <guido211@hotmail.it>
2024-05-05 17:27:24 +0200

    change

--- src/Library.groovy
+++ src/Library.groovy
@@ -32,6 +32,10 @@ class Library implements Serializable{
       script.sh 'git config --global user.email "jenkins@example.com"'
       script.sh 'git config --global user.name "jenkins"'
 
+      script.sh 'git status'
+      script.sh 'git branch'
+      script.sh 'git config --list'
+
       script.sh 'git remote set-url origin https://$USER:$PASS@github.com/guido88/java-maven-app.git'
       script.sh "git add ."
       script.sh 'git commit -m "ci: version bump"'
